3dd0aecf4b88326b9fc49880f18e736b8128794b
Pi Lanningham <pi.lanningham@gmail.com>
2023-11-25 11:57:51 -0500

    Add fraction type

---
+++ sundae-gql/Fraction.go
@@ -0,0 +1,25 @@
+package sundaeapi
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Fraction []int32
+
+func (Fraction) ImplementsGraphQLType(name string) bool {
+	return name == "Fraction"
+}
+
+func (a *Fraction) UnmarshalGraphQL(input interface{}) error {
+	f, ok := input.([]int32)
+	if !ok {
+		return fmt.Errorf("invalid fraction %v", input)
+	}
+	*a = f
+	return nil
+}
+
+func (a Fraction) MarshalJSON() ([]byte, error) {
+	return json.Marshal([]int32(a))
+}
