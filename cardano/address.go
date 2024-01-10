c66c9e05ae65e4c6087dcf6422bca35b6a3260c5
Pi Lanningham <pi.lanningham@gmail.com>
2024-01-09 21:38:39 -0500

    Return Byron Address Error

--- cardano/address.go
+++ cardano/address.go
@@ -2,11 +2,17 @@ package cardano
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/savaki/bech32"
 )
 
+var ErrByronAddress = fmt.Errorf("cannot split byron address")
+
 func SplitAddress(address string) (paymentCredential, stakingCredential []byte, err error) {
+	if !strings.HasPrefix(address, "addr") {
+		return nil, nil, ErrByronAddress
+	}
 	_, bytes, err := bech32.Decode(address)
 	if err != nil {
 		return nil, nil, fmt.Errorf("unable to decode address %v: %w", address, err)
