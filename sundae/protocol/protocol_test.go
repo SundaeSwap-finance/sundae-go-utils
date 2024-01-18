aee0a1de96f9164b806f05c9309a1aa0dbf4e4d6
Calvin Koepke <hello@calvinkoepke.com>
2024-01-18 13:33:32 -0700

    chore: add getLpToken utility

--- sundae/protocol/protocol_test.go
+++ sundae/protocol/protocol_test.go
@@ -47,3 +47,52 @@ func Test_DecodeProtocol(t *testing.T) {
 		t.Errorf("Incorrect blueprint validator 0 code: %x", protocol.Blueprint.Validators[0].CompiledCode)
 	}
 }
+
+func Test_GetLPToken(t *testing.T) {
+	v1ProtocolBytes := []byte(`{
+		"Version": "v1",
+		"Environment": "foo",
+		"Blueprint": {
+			"Validators": [
+				{
+					"Title": "pool.mint",
+					"Hash": "4086577ed57c514f8e29b78f42ef4f379363355a3b65b9a032ee30c9",
+					"CompiledCode": "0000000"
+				}
+			]
+		},
+		"References": {},
+		"Network": "testnet"
+	}`)
+
+	v3ProtocolBytes := []byte(`
+	{
+		"Version": "v1",
+		"Environment": "foo",
+		"Blueprint": {
+			"Validators": [
+				{
+					"Title": "pool.mint",
+					"Hash": "633a136877ed6ad0ab33e69a22611319673474c8bd0a79a4c76d9289",
+					"CompiledCode": "0000000"
+				}
+			]
+		},
+		"References": {},
+		"Network": "testnet"
+	  }
+	`)
+
+	var v1Protocol Protocol
+	v1Err := json.Unmarshal(v1ProtocolBytes, &v1Protocol)
+	if v1Err != nil {
+		t.Errorf("Failed to decode json: %v", v1Err)
+	}
+
+	var v3Protocol Protocol
+	v3Err := json.Unmarshal(v3ProtocolBytes, &v3Protocol)
+
+	if v3Err != nil {
+		t.Errorf("Failed to decode json: %v", v3Err)
+	}
+}
