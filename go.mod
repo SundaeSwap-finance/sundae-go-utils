00dd802728cead992002b93e99e50709071e574d
Pi Lanningham <pi.lanningham@gmail.com>
2024-08-21 01:47:44 -0400

    Add DAX wrapper
    Adds a wrapper that makes DAX work with newer aws client versions

--- go.mod
+++ go.mod
@@ -18,7 +18,8 @@ require (
 	github.com/SundaeSwap-finance/ogmigo/v6 v6.0.0-20240117201106-ce491d0b031e
 	github.com/SundaeSwap-finance/sundae-stats v0.0.0-20240122075805-b70d7eac501d
 	github.com/SundaeSwap-finance/sundae-sync v0.0.0-20231129062440-e3d0c42ddb69
-	github.com/aws/aws-sdk-go v1.49.13
+	github.com/aws/aws-dax-go v1.2.14
+	github.com/aws/aws-sdk-go v1.52.6
 	github.com/go-chi/chi/v5 v5.0.10
 	github.com/harlow/kinesis-consumer v0.3.5
 	github.com/savaki/bech32 v0.0.0-20220223220548-20f899656a90
@@ -34,10 +35,12 @@ require (
 )
 
 require (
+	github.com/antlr/antlr4 v0.0.0-20181218183524-be58ebffde8e // indirect
 	github.com/awslabs/kinesis-aggregation/go v0.0.0-20220610150308-f265332d248d // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fxamacker/cbor/v2 v2.5.0 // indirect
+	github.com/gofrs/uuid v4.4.0+incompatible // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/icza/bitio v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
@@ -45,7 +48,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/x448/float16 v0.8.4 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
---
+++ sundae-ddb/dax.go
@@ -0,0 +1,44 @@
+package sundaeddb
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-dax-go/dax"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+type DAXWrapper struct {
+	*dax.Dax
+}
+
+// These methods aren't implemented by the DAX library, meaning we can't use it as dynamodbiface
+// We don't really need them, so we just return an unimplemented error
+func (DAXWrapper) DeleteResourcePolicy(*dynamodb.DeleteResourcePolicyInput) (*dynamodb.DeleteResourcePolicyOutput, error) {
+	return nil, fmt.Errorf("unimplemented")
+}
+func (DAXWrapper) DeleteResourcePolicyWithContext(aws.Context, *dynamodb.DeleteResourcePolicyInput, ...request.Option) (*dynamodb.DeleteResourcePolicyOutput, error) {
+	return nil, fmt.Errorf("unimplemented")
+}
+func (DAXWrapper) DeleteResourcePolicyRequest(*dynamodb.DeleteResourcePolicyInput) (*request.Request, *dynamodb.DeleteResourcePolicyOutput) {
+	return nil, nil
+}
+func (DAXWrapper) GetResourcePolicy(*dynamodb.GetResourcePolicyInput) (*dynamodb.GetResourcePolicyOutput, error) {
+	return nil, fmt.Errorf("unimplemented")
+}
+func (DAXWrapper) GetResourcePolicyWithContext(aws.Context, *dynamodb.GetResourcePolicyInput, ...request.Option) (*dynamodb.GetResourcePolicyOutput, error) {
+	return nil, fmt.Errorf("unimplemented")
+}
+func (DAXWrapper) GetResourcePolicyRequest(*dynamodb.GetResourcePolicyInput) (*request.Request, *dynamodb.GetResourcePolicyOutput) {
+	return nil, nil
+}
+func (DAXWrapper) PutResourcePolicy(*dynamodb.PutResourcePolicyInput) (*dynamodb.PutResourcePolicyOutput, error) {
+	return nil, fmt.Errorf("unimplemented")
+}
+func (DAXWrapper) PutResourcePolicyWithContext(aws.Context, *dynamodb.PutResourcePolicyInput, ...request.Option) (*dynamodb.PutResourcePolicyOutput, error) {
+	return nil, fmt.Errorf("unimplemented")
+}
+func (DAXWrapper) PutResourcePolicyRequest(*dynamodb.PutResourcePolicyInput) (*request.Request, *dynamodb.PutResourcePolicyOutput) {
+	return nil, nil
+}
