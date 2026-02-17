21127f1fae39a98002547994288774fa083d12a0
Pi Lanningham <pi.lanningham@gmail.com>
2026-02-16 20:27:08 -0500

    Support websockets in our graphiql

--- graphiql/graphiql.go
+++ graphiql/graphiql.go
@@ -12,20 +12,32 @@ import (
 //go:embed graphiql.html
 var graphiql string
 
-// New endpoint is the url where you have your graphql api hosted
-func New(endpoint string) http.HandlerFunc {
+// Options configures the GraphiQL interface.
+type Options struct {
+	// SubscriptionURL is the WebSocket URL for GraphQL subscriptions (e.g. "wss://ws.example.com").
+	// If empty, subscriptions are disabled in the UI.
+	SubscriptionURL string
+}
+
+type templateVars struct {
+	Route           string
+	SubscriptionURL string
+}
+
+// New creates a GraphiQL handler. endpoint is the URL of the GraphQL HTTP API.
+func New(endpoint string, opts ...Options) http.HandlerFunc {
 	templ, err := template.New("graphiql").Parse(graphiql)
 	if err != nil {
 		panic(err)
 	}
+	vars := templateVars{Route: endpoint}
+	if len(opts) > 0 {
+		vars.SubscriptionURL = opts[0].SubscriptionURL
+	}
 	return func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
 		var buffer bytes.Buffer
-		var variables struct {
-			Route string
-		}
-		variables.Route = endpoint
-		if err := templ.Execute(&buffer, variables); err != nil {
+		if err := templ.Execute(&buffer, vars); err != nil {
 			fmt.Printf("Error: %v\n", err)
 			w.WriteHeader(http.StatusInternalServerError)
 			return
